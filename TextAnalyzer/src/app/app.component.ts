6d99eb988eb72d0e211502867c3e524e472230a5
MikaGEgger <mika.g.egger@gmail.com>
2025-05-15 20:42:31 +0200

    Added toggle button for offline/online check, results list and result details.

--- TextAnalyzer/src/app/app.component.ts
+++ TextAnalyzer/src/app/app.component.ts
@@ -1,53 +1,58 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {NgForOf, NgOptimizedImage} from '@angular/common';
 import {MatRadioButton, MatRadioGroup} from '@angular/material/radio';
 import {MatButton} from '@angular/material/button';
-import {MatFormField} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
 import {TextAnalyzer} from './textLogic/textAnalyzer';
+import {TextAnalyzerResult} from './textLogic/textAnalyzerResult';
+import {FormsModule} from '@angular/forms';
+import {MatSlideToggle, MatSlideToggleChange} from '@angular/material/slide-toggle';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgOptimizedImage, MatRadioGroup, MatRadioButton, MatButton, MatFormField, FormsModule, NgForOf],
+  imports: [RouterOutlet, NgOptimizedImage, MatRadioGroup, MatRadioButton, MatButton, NgForOf, FormsModule, MatSlideToggle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
 
   title = 'TextAnalyzer';
 
   textToBeAnalyzed!: string;
+  remoteCheck: boolean = false;
   vowelRadioChecked: boolean = false;
-  remoteRadioChecked: boolean = false;
   vowelCheckValue!: string;
-  remoteCheckValue!: string;
 
   textAnalyzer: TextAnalyzer = new TextAnalyzer;
-
-  results: string[][] = [["eins", "zwo"],["drei","vier"]];
+  results: TextAnalyzerResult [] = [];
+  resultDetails: string[] = [];
 
 
   onVowelRadioCheck(value: any) {
     this.vowelRadioChecked = true;
     this.vowelCheckValue = value;
   }
-  onRemoteRadioCheck(value: any) {
-    this.remoteRadioChecked = true;
-    this.remoteCheckValue = value;
+
+  onAnalyzeButtonClick() {
+    if (this.remoteCheck) {
+//TODO: Add Remote Check.
+    } else {
+      this.results.unshift(this.textAnalyzer.analyzeText(this.textToBeAnalyzed, this.vowelCheckValue == "Vowels"));
+    }
+    this.fillResultDetails(this.results[0].results);
   }
 
-  onAnalyzeButtonClick(){
-
-
-    if(this.remoteCheckValue == "Local"){
-      let result = this.textAnalyzer.analyzeText(this.textToBeAnalyzed, this.vowelCheckValue == "Vowels");
-      this.results.push(result);
-
+  fillResultDetails(results: Map<string, number>) {
+    this.resultDetails = [];
+    for (let [key, value] of results) {
+      this.resultDetails.push("Character: '" + key + "' occurs " + value + " times.");
     }
-    else{
+  }
 
-    }
+  onCalcToggleChanged(event: MatSlideToggleChange) {
+    this.remoteCheck = event.checked;
   }
 }
+
--- TextAnalyzer/src/app/textLogic/textAnalyzer.ts
+++ TextAnalyzer/src/app/textLogic/textAnalyzer.ts
@@ -1,26 +1,45 @@
+import {TextAnalyzerResult} from './textAnalyzerResult';
+
 export class TextAnalyzer{
 
-  public resultSet: string[] = [];
+  /*  For internationalization, we would either need to expand this list,
+      or change the data structure itself.
+      In this version things like á,ü,ì are not recognized as vowels.*/
+  private vowels :string[] = ['a','e','i','o','u'];
 
-
-  analyzeText(this: any, text: string, vowels: boolean) : string[]
+  analyzeText(this: any, text: string, vowels: boolean) : TextAnalyzerResult
   {
-        if(vowels)
-    {
-      this.resultSet = this.checkVowels(text);
-    }
-    else
-    {
-      this.resultSet = this.checkConsonants(text);
-    }
-    return this.resultSet;
-  }
-  checkVowels(text: string) {
-    return [text,"VowelsChecked"];
+      return new TextAnalyzerResult(text, this.createCharacterMap(text,vowels));
   }
 
-  checkConsonants(text: string[]) {
-    return [text,"ConsonantsChecked"];
+  createCharacterMap(text: string, vowels:boolean) {
+    let characterMap: Map<string, number> = new Map();
+    let undefinedValue = 0;
+
+    //if we search for vowels, we initialize all vowels in the map and only check for them.
+    //Also we set the undefinedValue to e
+    if (vowels) {
+      undefinedValue = -1;
+      this.vowels.forEach((character) => {
+        characterMap.set(character, 0);
+      });
+    }
+    for (let char of text) {
+      let count: number = characterMap.get(char) ?? undefinedValue
+      if (count >= 0) {
+        count= count+1;
+        characterMap.set(char, count);
+      }
+    }
+
+    //finally we remove the vowels from the list if we want consonant only.
+    if (!vowels) {
+      this.vowels.forEach((character) => {
+        characterMap.delete(character);
+      });
+    }
+
+    return characterMap;
   }
 
 }
