de2fd5dd1a4704090fb8e0f82b85271dca79be15
MikaGEgger <mika.g.egger@gmail.com>
2025-05-15 12:12:45 +0200

    [WIP] Initial result display.

--- TextAnalyzer/src/app/app.component.ts
+++ TextAnalyzer/src/app/app.component.ts
@@ -1,20 +1,53 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NgOptimizedImage} from '@angular/common';
+import {NgForOf, NgOptimizedImage} from '@angular/common';
 import {MatRadioButton, MatRadioGroup} from '@angular/material/radio';
 import {MatButton} from '@angular/material/button';
 import {MatFormField} from '@angular/material/form-field';
-import {CdkTextareaAutosize} from '@angular/cdk/text-field';
-import {MatInput} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {TextAnalyzer} from './textLogic/textAnalyzer';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgOptimizedImage, MatRadioGroup, MatRadioButton, MatButton, MatFormField, CdkTextareaAutosize, MatInput],
+  imports: [RouterOutlet, NgOptimizedImage, MatRadioGroup, MatRadioButton, MatButton, MatFormField, FormsModule, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'TextAnalyzer';
+
   textToBeAnalyzed!: string;
+  vowelRadioChecked: boolean = false;
+  remoteRadioChecked: boolean = false;
+  vowelCheckValue!: string;
+  remoteCheckValue!: string;
+
+  textAnalyzer: TextAnalyzer = new TextAnalyzer;
+
+  results: string[][] = [["eins", "zwo"],["drei","vier"]];
+
+
+  onVowelRadioCheck(value: any) {
+    this.vowelRadioChecked = true;
+    this.vowelCheckValue = value;
+  }
+  onRemoteRadioCheck(value: any) {
+    this.remoteRadioChecked = true;
+    this.remoteCheckValue = value;
+  }
+
+  onAnalyzeButtonClick(){
+
+
+    if(this.remoteCheckValue == "Local"){
+      let result = this.textAnalyzer.analyzeText(this.textToBeAnalyzed, this.vowelCheckValue == "Vowels");
+      this.results.push(result);
+
+    }
+    else{
+
+    }
+  }
 }
--- TextAnalyzer/src/app/textLogic/textAnalyzer.ts
+++ TextAnalyzer/src/app/textLogic/textAnalyzer.ts
@@ -2,9 +2,25 @@ export class TextAnalyzer{
 
   public resultSet: string[] = [];
 
+
   analyzeText(this: any, text: string, vowels: boolean) : string[]
   {
+        if(vowels)
+    {
+      this.resultSet = this.checkVowels(text);
+    }
+    else
+    {
+      this.resultSet = this.checkConsonants(text);
+    }
     return this.resultSet;
+  }
+  checkVowels(text: string) {
+    return [text,"VowelsChecked"];
+  }
+
+  checkConsonants(text: string[]) {
+    return [text,"ConsonantsChecked"];
   }
 
 }
