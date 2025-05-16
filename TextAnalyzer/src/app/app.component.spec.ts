bb868ff7642db6c812adbd702d16639a3ae9ff5c
MikaGEgger <mika.g.egger@gmail.com>
2025-05-16 13:32:04 +0200

    Added examples for tests

--- TextAnalyzer/src/app/app.component.spec.ts
+++ TextAnalyzer/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {TextAnalyzer} from './textLogic/textAnalyzer';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -20,10 +21,43 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('TextAnalyzer');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, TextAnalyzer');
+
+
+
+  /*
+  Those are just examples for tests, they do not include edge cases and show
+  limitations of the sample implementation.
+   */
+
+  it('should give vowels when checking for them.', () => {
+    let text = "Hello World!"
+    let textAnalyzer = new TextAnalyzer;
+    let result =textAnalyzer.analyzeText(text, true);
+
+    /*check if the checkedString is set correctly, if the key 'l' hasnt been incorrectly created
+     and if the value of the key 'o' corresponds to the expected number.
+     */
+    expect(result.checkedString==text);
+    expect(!result.results.has('l'));
+    expect(result.results.get('o')).toEqual(2);
   });
+
+  it('should create entries only for consonants when asked', () => {
+    let text = "Hello World!Ä"
+    let textAnalyzer = new TextAnalyzer;
+    let result =textAnalyzer.analyzeText(text, false);
+
+
+    expect(!result.results.has('e'));
+    expect(result.results.get('l')).toEqual(3);
+
+    expect(result.results.get('!')).toEqual(1); //Is this behaviour specified?
+
+    //Also this? -> if we strictly follow the logic from the textAnalyzer.java, then yes, but it seems incorrect to me.
+    expect(result.results.get('Ä')).toEqual(1);
+
+
+
+  });
+
 });
